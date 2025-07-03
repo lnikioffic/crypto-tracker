ca8c5f9e74a69e27f82396ab6be956c7c4770a59
lniki <nik_ivanov_33@mail.ru>
2025-07-03 23:41:14 +0500

    set base model metadata

--- backend-p/src/models.py
+++ backend-p/src/models.py
@@ -1,6 +1,18 @@
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
+# https://yandex.ru/search/?text=https%3A%2F%2Falembic.sqlalchemy.org%2Fen%2Flatest%2Fnaming.html&lr=55&clid=1836587
+convention: dict[str, str] = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=convention)
