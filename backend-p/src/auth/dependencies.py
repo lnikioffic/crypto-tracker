948761cb3f3d7bd88e263d4e90ca28ecc7501592
lniki <nik_ivanov_33@mail.ru>
2025-07-06 23:46:54 +0500

    add auth utils

---
+++ backend-p/src/auth/dependencies.py
@@ -0,0 +1,148 @@
+from typing import Annotated
+
+from fastapi import Depends, Form, HTTPException, Path, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.auth.schemas import TokenInfo, UserLogin, UserRead
+from src.auth.service import get_user_by_id, get_user_by_username
+from src.auth.token import (
+    TOKEN_TYPE_FIELD,
+    TokenType,
+    create_access_token,
+    create_refresh_token,
+)
+from src.auth.utils import decode_jwt, validate_password
+from src.database import DbSession
+
+error_found = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail='User not found',
+)
+
+
+async def create_token_jwt(user: UserRead) -> TokenInfo:
+    access_token = await create_access_token(user)
+    refresh_token = await create_refresh_token(user)
+
+    return TokenInfo(access_token=access_token, refresh_token=refresh_token)
+
+
+async def refresh_token_jwt(user: UserRead) -> TokenInfo:
+    access_token = await create_access_token(user)
+
+    return TokenInfo(access_token=access_token)
+
+
+async def delete_token_jwt(user: UserRead) -> TokenInfo:
+
+    return TokenInfo(access_token='', refresh_token=None)
+
+
+async def valid_user_username(user_name: str, session: AsyncSession) -> UserLogin:
+    user = await get_user_by_username(session, user_name)
+
+    if not user:
+        raise error_found
+
+    return user
+
+
+async def valid_user_id(
+    user_id: Annotated[int, Path], session: AsyncSession
+) -> UserRead | None:
+    user = await get_user_by_id(session, user_id)
+
+    if not user:
+        raise error_found
+
+    return user
+
+
+async def validate_create_user(
+    user: UserRead,
+) -> TokenInfo:
+
+    return await create_token_jwt(user)
+
+
+async def validate_auth_user_issue_jwt(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    db_session: DbSession,
+) -> TokenInfo:
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='invalid username or password',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    user: UserLogin = await valid_user_username(username, db_session)
+    if not user:
+        raise unauthed_exc
+
+    if not validate_password(
+        password=password,
+        hash_password=user.password,
+    ):
+        raise unauthed_exc
+
+    return await create_token_jwt(user)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+
+
+async def get_current_token_payload(
+    token: Annotated[str, Depends(oauth2_scheme)],
+) -> dict:
+    try:
+        payload = decode_jwt(token=token)
+    except InvalidTokenError as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token',
+            headers={'WWW-Authenticate': 'Bearer'},
+        ) from err
+    return payload
+
+
+async def validate_token_type(
+    payload: dict,
+    token_type: str,
+) -> bool:
+    current_token_type = payload.get(TOKEN_TYPE_FIELD)
+    if current_token_type == token_type:
+        return True
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f'invalid token type {current_token_type!r} expected {token_type!r}',
+    )
+
+
+async def get_current_auth_user_by_token_sub(
+    db_session: DbSession,
+    payload: Annotated[dict, Depends(get_current_token_payload)],
+) -> UserRead:
+    await validate_token_type(payload=payload, token_type=TokenType.ACCESS_TOKEN_TYPE)
+    id: str | None = payload.get('sub')
+    if user := await valid_user_id(int(id), db_session):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='token invalid (user not found)',
+    )
+
+
+async def get_current_auth_user_for_refresh(
+    session: AsyncSession,
+    payload: Annotated[dict, Depends(get_current_token_payload)],
+) -> UserRead:
+    await validate_token_type(payload=payload, token_type=TokenType.REFRESH_TOKEN_TYPE)
+    id: str | None = payload.get('sub')
+    if user := await valid_user_id(int(id), session):
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='token invalid (user not found)',
+    )
---
+++ backend-p/src/auth/token.py
@@ -0,0 +1,60 @@
+from datetime import timedelta
+from enum import StrEnum
+
+from src.auth.config import auth_jwt_settings
+from src.auth.schemas import UserRead
+from src.auth.utils import encode_jwt
+
+TOKEN_TYPE_FIELD = 'type'
+
+
+class TokenType(StrEnum):
+    ACCESS_TOKEN_TYPE = 'access'
+    REFRESH_TOKEN_TYPE = 'refresh'
+
+
+async def create_token(
+    token_type: str,
+    payload: dict,
+    expire_minutes: int = auth_jwt_settings.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+
+    jwt_payload = {TOKEN_TYPE_FIELD: token_type}
+
+    jwt_payload.update(payload)
+
+    token = 'Bearer ' + encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
+    return token
+
+
+async def create_access_token(user: UserRead) -> str:
+    payload = {
+        'sub': str(user.id),
+        'user': {
+            'username': user.username,
+            'email': user.email,
+        },
+    }
+
+    return await create_token(
+        token_type=TokenType.ACCESS_TOKEN_TYPE,
+        payload=payload,
+        expire_minutes=auth_jwt_settings.access_token_expire_minutes,
+    )
+
+
+async def create_refresh_token(user: UserRead) -> str:
+    payload = {
+        'sub': str(user.id),
+    }
+
+    return await create_token(
+        token_type=TokenType.REFRESH_TOKEN_TYPE,
+        payload=payload,
+        expire_timedelta=timedelta(days=auth_jwt_settings.refresh_token_expire_days),
+    )
