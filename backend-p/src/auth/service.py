38308cff4e58cfd7bcf797181f7e454b40556bd7
lniki <nik_ivanov_33@mail.ru>
2025-07-06 23:47:35 +0500

    add service user

---
+++ backend-p/src/auth/service.py
@@ -0,0 +1,44 @@
+from fastapi import HTTPException, status
+from pydantic import EmailStr
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from src.auth.models import User
+from src.auth.schemas import UserCreate, UserLogin, UserRead
+from src.auth.utils import hash_password
+
+
+async def get_user_by_username(session, username: str) -> UserLogin | None:
+    stmt = select(User).filter(User.username == username)
+    result: Result = await session.execute(stmt)
+    user = result.scalar()
+    return user
+
+
+async def get_user_by_id(session, id: int) -> User | None:
+    user = await session.get(User, id)
+    return user
+
+
+async def get_user_by_email(session, email: EmailStr) -> UserLogin | None:
+    stmt = select(User).filter(User.email == email)
+    result = await session.execute(stmt)
+    user = result.scalar()
+    return user
+
+
+async def create_user(session, user: UserCreate) -> UserRead:
+
+    email_exist = await get_user_by_email(session, user.email)
+    username_exist = await get_user_by_username(session, user.username)
+
+    if email_exist is not None or username_exist is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User with this email or username already exist',
+        )
+
+    user.password = hash_password(user.password).decode()
+    add_user = User(**user.model_dump())
+    session.add(add_user)
+    await session.commit()
+    return add_user
