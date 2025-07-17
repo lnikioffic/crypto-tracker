5b824051c4fbef44f6a6e89c4ae897529a6be25c
lniki <nik_ivanov_33@mail.ru>
2025-07-17 15:55:06 +0500

    fix redirect

--- frontend/src/stores/cryptoStore.ts
+++ frontend/src/stores/cryptoStore.ts
@@ -6,7 +6,7 @@ import { useCurrencyStore } from "./currencyStore";
 export async function fetchCryptoList(): Promise<CryptoData[]> {
   const { currency } = useCurrencyStore.getState();
 
-  const response = await api.get<CryptoData[]>("/coins", {
+  const response = await api.get<CryptoData[]>("/coins/", {
     params: {
       vs_currency: currency.toLowerCase(),
     },
