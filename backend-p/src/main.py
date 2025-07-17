d1a79da319726f2e53cb3b6670531dce850b9a85
lniki <nik_ivanov_33@mail.ru>
2025-07-17 15:57:27 +0500

    add GZipMiddleware

--- backend-p/src/main.py
+++ backend-p/src/main.py
@@ -1,16 +1,15 @@
-from contextlib import asynccontextmanager
 import logging
-import sys
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from src.api_coins.redis_client import RedisRepository
 from src.api_coins.router import coin_router
 from src.auth.router import auth_router, user_router
 from src.database import db
-from src.portfolio.router import portfolio_router
 from src.logging import configure_logging
-
+from src.portfolio.router import portfolio_router
 
 log = logging.getLogger(__name__)
 configure_logging()
@@ -18,14 +17,11 @@ configure_logging()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    try:
-        client = RedisRepository()
-        response = await client._client.ping()
-        print(f"Redis connection status: {response}")
-        log.info(f"Redis connection status: {response}")
-    except:
-        log.error(' Application startup failed. Exiting.', exc_info=True)
-        sys.exit(1)
+    client = RedisRepository()
+    response = await client._client.ping()
+    print(f"Redis connection status: {response}")
+    log.info(f"Redis connection status: {response}")
+    log.error(' Application startup failed. Exiting.', exc_info=True)
     yield
     await db.close()
 
@@ -43,10 +39,16 @@ app.add_middleware(
 
 @app.middleware("http")
 async def check_cookies(request: Request, call_next):
-    # print("Cookies:", request.cookies)  # Для отладки
+    
+    log.info(f"Request started: {request.method} {request.url}")
     response = await call_next(request)
+    log.info(
+        f'Request completed: {request.method} {request.url} - Status: {response.status_code}'
+    )
     return response
 
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 app.include_router(auth_router)
 app.include_router(user_router)
